cb8642c89385d544e7db2f08bbeb85aa378cf324
justin-K4Dev <127049870+justin-K4Dev@users.noreply.github.com>
2023-03-12 02:26:44 +0900

    Add files via upload

---
+++ TestMulticore/MSVS/Async-CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AsyncAwaitExample1.Test();
+
+            AsyncAwaitExample2.Test();
+        }
+
+    }
+}
---
+++ TestMulticore/MSVS/Async-CSharp/async-await/AsyncAwaitExample2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class AsyncAwaitExample2
+    {
+        public static void Test()
+        {
+            // Start computation.
+            AllocateAsync();
+            // Handle user input.
+            string result = Console.ReadLine();
+            Console.WriteLine("You typed: " + result);
+        }
+
+        static async void AllocateAsync()
+        {
+            // This method runs asynchronously.
+            int t = await Task.Run(() => Allocate());
+            Console.WriteLine("Compute: " + t);
+        }
+
+        static int Allocate()
+        {
+            // Compute total count of digits in strings.
+            int size = 0;
+            for (int z = 0; z < 100; z++)
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    string value = i.ToString();
+                    if (value == null)
+                    {
+                        return 0;
+                    }
+                    size += value.Length;
+                }
+            }
+            return size;
+        }
+    }
+}
